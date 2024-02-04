c24fbc7e0c72eeeb629e0c3eb13f65e13adc572a
Vladimir Kirpichev <42208172+djbobah@users.noreply.github.com>
2024-02-04 20:44:43 +0300

    1

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,22 +1,26 @@
 import * as React from "react";
 import { useState } from "react";
 
+import logo from "../../assets/img/logo_EdSpase_white.svg";
 import "./modal.scss";
 
-export default ({ setOpenModal, openModal }) => {
+const Modal = ({ setOpenModal, openModal }) => {
   const [name, setName] = useState("");
-  const [age, setAge] = useState(14);
+  const [organization, setOrganization] = useState("");
+  const [position, setPosition] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
   const closeHandler = () => {
     setOpenModal(false);
-    setAge(14);
+    // setAge(14);
     setName("");
   };
 
   const handler = (e) => {
     const data = {
       name,
-      age,
+      // age,
     };
     console.log(data);
     closeHandler();
@@ -28,15 +32,41 @@ export default ({ setOpenModal, openModal }) => {
         <div className="modal__close" onClick={closeHandler}>
           <i className="fa fa-times" />
         </div>
+        <img src={logo} className="modal__logo" alt="Logo" />
         <div className="modal__content">
-          <label>
-            <span>Имя</span>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
+          {/* <label> */}
+          {/* <span>Имя</span> */}
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Имя Фамилия"
+          />
+          <input
+            type="text"
+            value={organization}
+            onChange={(e) => setOrganization(e.target.value)}
+            placeholder="Название организации"
+          />
+          <input
+            type="text"
+            value={position}
+            onChange={(e) => setPosition(e.target.value)}
+            placeholder="Должность"
+          />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <input
+            type="tel"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            placeholder="Телефон"
+          />
+          {/* </label>
           <label>
             <span>Возраст</span>
             <input
@@ -44,12 +74,13 @@ export default ({ setOpenModal, openModal }) => {
               max={99}
               min={14}
               value={age}
-              onChange={(e) => setAge(e.target.value)}
+              onChange={(e) => setAge(Number(e.target.value))}
             />
-          </label>
+          </label> */}
           <button onClick={handler}>Отправить</button>
         </div>
       </div>
     </div>
   );
 };
+export default Modal;
