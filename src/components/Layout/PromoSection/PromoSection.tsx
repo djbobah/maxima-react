7730a644ca11a1ca0c83597579988149ca16e6c1
Vladimir <djbobah@mail.ru>
2024-02-12 16:11:23 +0300

    done without animation

--- src/components/Layout/PromoSection/PromoSection.tsx
+++ src/components/Layout/PromoSection/PromoSection.tsx
@@ -11,14 +11,13 @@ interface PromoSectionProps {
 }
 
 const PromoSection = ({ setOpenModal }: PromoSectionProps) => {
-  // const [data, setData] = React.useState(CarouserData);
   const [activeSlide, setActiveSlide] = React.useState(0);
 
   React.useEffect(() => {
-    if (activeSlide > CarouserData.length) {
+    if (activeSlide > CarouserData.length - 1) {
       setActiveSlide(0);
     } else if (activeSlide < 0) {
-      setActiveSlide(CarouserData.length);
+      setActiveSlide(CarouserData.length - 1);
     }
   }, [activeSlide]);
 
@@ -30,7 +29,7 @@ const PromoSection = ({ setOpenModal }: PromoSectionProps) => {
         onClick={() => setActiveSlide((prev) => Number(prev) - 1)}
       />
       <div className="field_slider">
-        {CarouserData.map(
+        {/* {CarouserData.map(
           (
             item: { title: string; content: string; img: string },
             index: number
@@ -53,31 +52,30 @@ const PromoSection = ({ setOpenModal }: PromoSectionProps) => {
               activeSlide,
               "index",
               index
-            );
-
-            return (
-              <div className={`slider ${slide}`} key={index}>
-                {" "}
-                {/*  */}
-                <div className="promo__textBlock">
-                  <h1 className="promo__title">
-                    Ed Space
-                    <br />
-                    {item?.title}
-                  </h1>
-                  <p className="promo__description">{item.content}</p>
-                </div>
-                <div>
-                  <img
-                    className="promo__img"
-                    src={`/assets/img/${item?.img}`}
-                    alt="PromoImg"
-                  />
-                </div>
-              </div>
-            );
-          }
-        )}
+            ); */}
+        {/* return ( */}
+        <div className={`slider `}>
+          <div className="promo__textBlock">
+            <h1 className="promo__title">
+              Ed Space
+              <br />
+              {CarouserData[activeSlide]?.title}
+            </h1>
+            <p className="promo__description">
+              {CarouserData[activeSlide]?.content}
+            </p>
+          </div>
+          <div>
+            <img
+              className="promo__img"
+              src={`/assets/img/${CarouserData[activeSlide]?.img}`}
+              alt="PromoImg"
+            />
+          </div>
+        </div>
+        {/* ); */}
+        {/* } */}
+        {/* )} */}
       </div>
       <div>
         <Button
