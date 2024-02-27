247ff514d0d580eac7fd828da6cbbd3c1120355d
Vladimir Kirpichev <42208172+djbobah@users.noreply.github.com>
2024-02-27 20:28:31 +0300

    1

--- src/components/CardWeather/cardWeather.tsx
+++ src/components/CardWeather/cardWeather.tsx
@@ -1,25 +1,42 @@
 import * as React from "react";
-import { Card, Flex, Space } from "antd";
+import { Card, Flex, Space, Typography } from "antd";
 
+import { blue } from "@ant-design/colors";
+const { Text } = Typography;
 const { Meta } = Card;
 
 export default function CardWeather({ town }) {
   console.log(town.weather[0].icon);
 
   //https://api.unsplash.com/search/photos?page=1&query=office&client_id=HDZ3h9US_THNtSh9fk1Z04tHPVytkLtRTQDdV_rD4gw
-  https: return (
+  // https:
+  return (
     <Card hoverable style={{}}>
       {/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
       <h2> {town.nameRUS}</h2>
       <img
         src={`https://openweathermap.org/img/wn/${town.weather[0].icon}.png`}
+        alt="icon weather"
       />
-      <div>
-        Температура {(Number(town.main.feels_like) - 273.15).toFixed(1)}
-      </div>
       <Flex justify="space-between">
-        <span>Ощущается</span>{" "}
-        <span>{(Number(town.main.temp) - 273.15).toFixed(1)}</span>
+        <span> Температура</span>
+        {Number(town.main.temp) - 273.15 >= 0 ? (
+          <Text>{(Number(town.main.temp) - 273.15).toFixed(1)} ºC </Text>
+        ) : (
+          <Text style={{ color: "#1890ff" }}>
+            {(Number(town.main.temp) - 273.15).toFixed(1)} ºC{" "}
+          </Text>
+        )}
+      </Flex>
+      <Flex justify="space-between">
+        <span>Ощущается</span>
+        {Number(town.main.temp) - 273.15 >= 0 ? (
+          <Text>{(Number(town.main.feels_like) - 273.15).toFixed(1)} ºC </Text>
+        ) : (
+          <Text style={{ color: "#1890ff" }}>
+            {(Number(town.main.feels_like) - 273.15).toFixed(1)} ºC{" "}
+          </Text>
+        )}
       </Flex>
     </Card>
   );
